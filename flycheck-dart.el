0a84892a7484c4111837d208b4be92b1ca9501f3
martin.haesler <martin.haesler@gmail.com>
2015-04-13 14:11:38 +0100

    Add package-requires text

--- flycheck-dart.el
+++ flycheck-dart.el
@@ -5,13 +5,34 @@
 ;; Author: martin haesler
 ;; Keywords: tools, convenience
 ;; Version: 0.0.1
+;; Package-Requires: ((flycheck "0.22") (dart-mode "0.11"))
+
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of Emacs.
+
+;;; Commentary:
+
+;;; Code:
 
 (require 'flycheck)
 
 (flycheck-define-checker dart
   "A Python syntax and style checker using Pylint. "
   :command ("dartanalyzer" "--format=machine" source)
-  :error-patterns 
+  :error-patterns
  ((error line-start "ERROR" "|" (message) "|" 
           (file-name) "|" line "|"  column "|" column "|" line-end)
 
